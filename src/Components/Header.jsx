35ace0262871b8043aa376776f3555a7f08af801
Anagha <anaghalakshmeeka@gmail.com>
2024-02-15 17:15:44 +0530

    navbar fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,23 +4,25 @@ import { Link } from 'react-router-dom'
 
 function Header() {
     return (
-        <div className='sticky-top'>
-            <Navbar bg="dark" data-bs-theme="dark">
+        // <div className='sticky-top'>
+            <Navbar bg="dark" data-bs-theme="dark" className='w-100 sticky-top' expand='lg'>
                 <Container>
                     <Navbar.Brand>
                         <Link to={'/'} style={{ textDecoration: 'none',color:"white" }}>
                             <h1><i className="fa-solid fa-graduation-cap"></i>Education</h1>
                         </Link>
                     </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link><Link to={'/'}>Home</Link></Nav.Link>
                         <Nav.Link><Link to={'/register'}>Register</Link></Nav.Link>
                         <Nav.Link><Link to={'/students'}>Students</Link></Nav.Link>
-
                     </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
-        </div>
+        // </div>
     )
 }
 
